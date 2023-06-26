73b73d6728a8e511e2a97cb8d851c2ce0482cc34
Tarun Yadav <tarun007.b@gmail.com>
2023-06-26 13:13:16 +0530

    first react app

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+       This is about components.
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad iure placeat id eum, similique eos molestias odit dolorem, cum neque, voluptas sint architecto voluptate! In numquam, laboriosam debitis maiores itaque dolorem odit temporibus accusantium!</p>
+    </div>
+  )
+}
+
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export const Footer = () => { 
+    
+    return (
+        <footer className="bg-dark text-light py-1">
+            <p className='text-center my-2'>
+            Created By: Tarun Yadav 
+            </p>
+        </footer>
+    )
+}
